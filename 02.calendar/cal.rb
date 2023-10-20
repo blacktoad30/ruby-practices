3faaf9fb5d016191fa9a4c6d512aa4625e0fd4f6
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2023-10-20 23:18:31 +0900

    Add the source code that solved the calendar problem
    * 02.calendar/cal.rb: New file.

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'date'
+
+params = ARGV.getopts("m:y:")
+#p params
+year = params["y"].to_i
+month = params["m"].to_i
+#p month, year
+
+this_monthp = if year.zero? && month.zero? then true else false end
+today = Date.today
+if year.zero? || month.zero?
+  at_year = if year.zero? then today.year else year end
+  at_month = if month.zero? then today.month else month end
+end
+#p today, at_month, at_year
+exit 1 unless (at_month.between?(1, 12) && at_year.between?(1970, 2100))
+at_first_wday = Date.new(at_year, at_month, 1).wday
+at_last_day = Date.new(at_year, at_month, -1).day
+mdays = Array.new(6) { Array.new(7, "  ") }
+dayc = 1
+mdays.size.times do |weekc|
+  next if dayc > at_last_day
+  mdays[weekc].size.times do |wd|
+    if weekc == 0 && wd >= at_first_wday || weekc > 0 && dayc <= at_last_day
+      if this_monthp && dayc == today.day
+        mdays[weekc][wd] = "\e[7m" + dayc.to_s.rjust(2) + "\e[m"
+      else
+        mdays[weekc][wd] = dayc.to_s.rjust(2)
+      end
+      dayc += 1
+    end
+  end
+end
+#p mdays
+
+puts "#{at_month}月 #{at_year}".center(20).delete_suffix!(" ")
+puts "日 月 火 水 木 金 土"
+mdays.each do |week|
+  puts week.join(" ")
+end
