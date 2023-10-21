ece6dcdb76234e667bba1ef3d03be64516c2540c
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2023-10-21 13:45:31 +0900

    Determine whether it is this month
    * 02.calendar/cal.rb: Correct the condition.

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,12 +9,13 @@ year = params["y"].to_i
 month = params["m"].to_i
 #p month, year
 
-this_monthp = if year.zero? && month.zero? then true else false end
 today = Date.today
 if year.zero? || month.zero?
   at_year = if year.zero? then today.year else year end
   at_month = if month.zero? then today.month else month end
 end
+this_monthp = if at_year == today.year && at_month == today.month \
+              then true else false end
 #p today, at_month, at_year
 exit 1 unless (at_month.between?(1, 12) && at_year.between?(1970, 2100))
 at_first_wday = Date.new(at_year, at_month, 1).wday
