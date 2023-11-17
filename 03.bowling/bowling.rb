84d2a8ae17991ea63e0f954269889e4f47211c64
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2023-11-17 17:21:48 +0900

    Change some variables
    * 03.bowling/bowling.rb (last_frame): Rename from 'scores'.
    (frames): Remove it.

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,11 +5,11 @@ MAX_PINS = 10
 shots = ARGV[0].split(',').flat_map do |s|
   s == 'X' ? [MAX_PINS, nil] : s.to_i
 end
-scores = shots.slice(2 * 9, shots.size).compact.each_slice(2).to_a
-scores.push([0]) if scores.size == 1
-frames = shots.take(2 * 9).each_slice(2).map(&:compact) + scores
-scores = frames.each_cons(3).map do |trio|
+last_frame = shots.slice(2 * 9, shots.size).compact.each_slice(2).to_a
+# 最終フレームに3投目が無くても、each_consメソッドでのスコア処理を可能にする。
+last_frame.push([0]) if last_frame.size == 1
+scores = (shots.take(2 * 9).each_slice(2).map(&:compact) + last_frame).each_cons(3).map do |trio|
   trio.first == [MAX_PINS] || trio.first.sum == MAX_PINS ? trio.flatten.take(3) : trio.first
-end + scores
+end + last_frame
 
 puts scores.flatten.sum
