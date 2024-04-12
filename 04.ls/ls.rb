ac90fe1549424579d79c199d57d6a6ea7fc547fd
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2024-04-12 21:54:38 +0900

    Support '-a' option
    * 04.ls/lib/ls_methods.rb (#child_files): Add 'export_all' keyword
    parameter.
    
    * 04.ls/ls.rb: Get '-a' option from ARGV.

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,5 +2,8 @@
 # frozen_string_literal: true
 
 require_relative './lib/ls_methods'
+require 'optparse'
 
-table_print(child_files('.'), 3)
+opts = ARGV.getopts('a')
+
+table_print(child_files('.', export_all: opts['a']), 3)
