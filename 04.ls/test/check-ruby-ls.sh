adeff0598f2708a3f4231e68ae979eca289c2ffe
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2024-12-10 03:52:28 +0900

    Format check script
    * 04.ls/test/check-ruby-ls.sh: Cleanup and fix typo.  Reduce options.

--- 04.ls/test/check-ruby-ls.sh
+++ 04.ls/test/check-ruby-ls.sh
@@ -3,77 +3,78 @@ set -eu
 
 usage() {
   cat <<-EOF
-Usage: ${0} [OPTION] ...
+Usage: ${0##*/} [-P LS_OPTS] [-W COL_NUM] [-V] ...
 
-  -v         \`set -v\`
-  -x         \`set -x\`
-  -V         verbose output
-  -a         list all entries
-  -r         reverse list order
-  -w[WIDTH]  adjust output width, default is 0 (ignore with '-l')
-  -l         list in long format
+Options:
+
+  -P LS_OPTS  'ls' command options string
+  -W COL_NUM  increase output width by COL_NUM (ignore by '-l')
+  -V          verbose output
 EOF
   exit ${1:-0}
 }
 
-opts='vxVarlw:'
-verbose=0
-
-while
-  getopts "${opts}" opt
-do
-  case "$opt" in
-  (v|x) set -${opt};;
-  (V) verbose=1;;
-  (a|r|l) ls_opts="${ls_opts:+$ls_opts }-$opt";;
-  (w) ext_width="$OPTARG";;
-  (?) usage;;
-  esac
-  case "$opt" in
-  (l) ls_islongfmt=1;;
-  esac
-done
-shift $(($OPTIND - 1))
+clean() {
+  errno=$?
+  trap '' EXIT HUP INT QUIT PIPE ALRM TERM
+  test -d "$1" && rm -dr "$1"
+  trap - EXIT HUP INT QUIT PIPE ALRM TERM
+  exit $errno
+}
 
 max_width() {
-  awk 'length > max { max = length } END { print max + ext_width }' ext_width="${1:-0}"
+  awk '
+  length > max { max = length }
+  END { print max + 1 + ext_width }
+  ' ext_width="${1:-0}"
 }
+
+while
+  getopts P:W:V opt
+do
+  case "$opt" in
+  (P) ls_opts="${ls_opts:+$ls_opts }$OPTARG";;
+  (W) ext_width="$OPTARG";;
+  (V) verbose=1;;
+  (?) usage 1;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+: "${ls_opts:=}" "${ext_width:=0}" "${verbose:=0}"
+
+case "$ls_opts" in
+("${ls_opts##*l}") ls_islongfmt=0;;
+(*) ls_islongfmt=1;;
+esac
 
 proj_root="$(cd $(dirname $0)/../..; echo $PWD)"
-
 test_dir="/tmp/.test_ruby_ls"
 
-clean() {
-  set -- ${1:-$?}
-  trap '' EXIT HUP INT QUIT PIPE ALRM TERM
-  test -d "$test_dir" && rm -dr "$test_dir"
-  trap - EXIT HUP INT QUIT PIPE ALRM TERM
-  exit $1
-}
-
-trap 'clean' EXIT HUP INT QUIT PIPE ALRM TERM
+trap "clean '$test_dir'" EXIT HUP INT QUIT PIPE ALRM TERM
 
 mkdir "$test_dir"
 
 cmd_ls="ls${ls_opts:+ ${ls_opts}}"
 cmd_ruby_ls="${proj_root}/04.ls/ls.rb${ls_opts:+ ${ls_opts}}"
 
-(exit $verbose) || echo "# cwd: ${PWD}" 1>&2
+case $verbose in
+(0) :;;
+(*) echo "# cwd: ${PWD}" 1>&2;;
+esac
 
-case ${ls_islongfmt:-0} in
+case $ls_islongfmt in
 (0)
   mkfifo "$test_dir/.ruby-ls.0.fifo" "$test_dir/.ruby-ls.1.fifo"
 
   ${cmd_ruby_ls} |
-    if
-      ! (exit $verbose)
-    then
-      tee "$test_dir/.ruby-ls.0.fifo" "$test_dir/.ruby-ls.1.fifo" 1>&2 &
-    else
-      tee "$test_dir/.ruby-ls.0.fifo" "$test_dir/.ruby-ls.1.fifo" >/dev/null &
-    fi
+    case $verbose in
+    (0) tee "$test_dir/.ruby-ls.0.fifo" "$test_dir/.ruby-ls.1.fifo" >/dev/null &;;
+    (*) tee "$test_dir/.ruby-ls.0.fifo" "$test_dir/.ruby-ls.1.fifo" 1>&2 &;;
+    esac
 
-  LC_ALL=C ${cmd_ls} -C -w$(max_width "${ext_width:-1}" <"$test_dir/.ruby-ls.1.fifo") |
+  LC_ALL=C ${cmd_ls} -C -w$(max_width "$ext_width" <"$test_dir/.ruby-ls.1.fifo") |
     expand -t8 |
     diff -u - "$test_dir/.ruby-ls.0.fifo"
   ;;
@@ -83,15 +84,12 @@ case ${ls_islongfmt:-0} in
   tee "$test_dir/.ls.1.fifo" <"$test_dir/.ls.0.fifo" >/dev/null &
 
   ${cmd_ruby_ls} |
-    if
-      ! (exit $verbose)
-    then
-      tee "$test_dir/.ruby-ls.0.fifo" 1>&2 &
-    else
-      tee "$test_dir/.ruby-ls.0.fifo" >/dev/null &
-    fi
+    case $verbose in
+    (0) tee "$test_dir/.ruby-ls.0.fifo" >/dev/null &;;
+    (*) tee "$test_dir/.ruby-ls.0.fifo" 1>&2 &;;
+    esac
 
-  LC_ALL=C ls -l |
+  LC_ALL=C ${cmd_ls} |
     tee "$test_dir/.ls.0.fifo" |
     if
       grep -Eq '^[-[:alpha:]]{10}[^ ]'
