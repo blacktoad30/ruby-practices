f24baea163a2a1f90f7d77f0ac382d68763e00fa
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2025-01-10 15:35:58 +0900

    fixup! *Save*

--- 05.wc/test/test_wc_methods.rb
+++ 05.wc/test/test_wc_methods.rb
@@ -6,14 +6,26 @@ require 'logger'
 require 'yaml'
 require_relative '../lib/wc_methods'
 
+LOGGER_LEVEL =
+  Logger::Severity.constants.to_h do |const_sym|
+    [const_sym.to_s, Logger::Severity.const_get(const_sym)]
+  end
+
 LOGGER = Logger.new($stdout)
-LOGGER.level = Logger::DEBUG
+
+LOGGER.level = LOGGER_LEVEL[ARGV[0]] || ARGV[0]&.to_i || Logger::WARN
+
+TEST_DATA_SETS_DIR = "#{File.dirname(__FILE__)}/data/".freeze
 
 # test 'wc_methods.rb'
 class WcMethodsTest < Minitest::Test
+  def data_sets_file_path(filename)
+    "#{TEST_DATA_SETS_DIR}#{filename}.yaml"
+  end
+
   def test_wc_parse_args
     test_data_sets =
-      YAML.load_file("#{File.dirname(__FILE__)}/data/wc_parse_args.yaml")
+      YAML.load_file(data_sets_file_path(__method__.to_s.gsub(/^test_/, '')))
 
     test_data_sets.each do |test_data_set|
       args = test_data_set['args']
@@ -23,25 +35,29 @@ class WcMethodsTest < Minitest::Test
       print_opts, operands = wc_parse_args(args)
 
       LOGGER.debug(__method__) { "print_opts: #{print_opts}" }
-
-      assert_equal(print_opts, test_data_set['print_opts'])
-
       LOGGER.debug(__method__) { "operands: #{operands}" }
 
+      assert_equal(print_opts, test_data_set['print_opts'])
       assert_equal(operands, test_data_set['operands'])
     end
   end
 
-  # def test_parse_operands
-  #   test_data_sets =
-  #     YAML.load_file("#{File.dirname(__FILE__)}/data/parse_operands.yaml")
+  def test_wc_count_by_type_from_io
+    test_data_sets =
+      YAML.load_file(data_sets_file_path(__method__.to_s.gsub(/^test_/, '')))
 
-  #   test_data_sets.each do |test_data_set|
-  #     args = test_data_set['args'].dup
+    test_data_sets.each do |test_data_set|
+      input = test_data_set['input']
 
-  #     operands = parse_operands(args)
+      LOGGER.debug(__method__) { "input: #{input}" }
 
-  #     assert_equal(operands, test_data_set['operands'])
-  #   end
-  # end
+      io = StringIO.new(input)
+
+      count_by_type = wc_count_by_type_from_io(io)
+
+      LOGGER.debug(__method__) { "count_by_type: #{count_by_type}" }
+
+      assert_equal(count_by_type, test_data_set['count_by_type'])
+    end
+  end
 end
