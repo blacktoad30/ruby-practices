d09bfa9f5b797f93c0e0b056a4bdb7302e3c9a46
Keisuke Kurosawa <k.kurochan9630@gmail.com>
2025-05-15 15:44:22 +0900

    feat: Define `WordCount` module
    * 05.wc/lib/word_count.rb: New file.
    (WordCount, WordCount::IO, WordCount::String): New module.
    (IO): Mix-in `WordCount::IO`.
    (String): Mix-in `WordCount::String`.

---
+++ 05.wc/lib/word_count.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module WordCount
+  TYPES = %i[newline word byte].freeze
+
+  def extract_types(types)
+    TYPES & types
+  end
+
+  module_function :extract_types
+end
+
+module WordCount::IO
+  BUFFER_SIZE = 16 * 1024
+
+  private_constant :BUFFER_SIZE
+
+  def word_count(word_count_types = WordCount::TYPES, file_size = nil)
+    counts = []
+
+    loop do
+      str = readpartial(BUFFER_SIZE)
+
+      count = str.word_count(word_count_types, file_size)
+
+      counts << count
+    rescue EOFError
+      break
+    end
+
+    word_count_types.to_h { [_1, _1 == :byte ? file_size.to_i : 0] }
+                    .merge!(*counts) { |_, total, count| total + count }
+  end
+end
+
+module WordCount::String
+  def word_count(word_count_types = WordCount::TYPES, file_size = nil)
+    word_count_types.to_h { [_1, _1 == :byte && !file_size.nil? ? 0 : word_count_per_type(_1)] }
+  end
+
+  private
+
+  def word_count_per_type(word_count_type)
+    case word_count_type
+    when :newline
+      count("\n")
+    when :word
+      split.size
+    when :byte
+      bytesize
+    else
+      raise(ArgumentError, "word_count_type: allow only #{WordCount::TYPES.map(&:inspect).join(', ')}")
+    end
+  end
+end
+
+class IO
+  include WordCount::IO
+end
+
+class String
+  include WordCount::String
+end
